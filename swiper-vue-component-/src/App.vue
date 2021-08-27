1a0af37bf50d7fecadfe6124638689bba5d15cbc
ASYCN\Fangyan.Li <lfy@qq.com>
2021-08-27 10:59:55 +0800

    组件

---
+++ swiper-vue-component-/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <Swiper :infinityLoop="true" pagination="dot">
+      <SwiperItem v-for="(item, index) in arr" :key="index">
+          <img :src="item" />
+      </SwiperItem>
+    </Swiper>
+  </div>
+</template>
+
+<script>
+import Swiper  from "./components/Swiper";
+import  SwiperItem  from "./components/SwiperItem.vue";
+
+export default {
+  name: 'App',
+  components: {
+      Swiper,
+      SwiperItem
+  },
+  data () {
+    return{
+      arr: [ require('./assets/nature-1.jpg'), 
+      require('./assets/nature-2.jpg'), 
+      require('./assets/nature-3.jpg'),
+      require('./assets/nature-4.jpg')]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+    position: relative;
+    height: 100%;
+}
+</style>
